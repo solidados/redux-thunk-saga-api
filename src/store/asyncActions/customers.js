56fa8197668b63602448ed8cc00d49c1b558c7fb
solidados <geekbrains.pk@internet.ru>
2024-05-20 15:47:19 +0400

    feat: create fetchCustomers function and import addCustomersListAction in it

---
+++ src/store/asyncActions/customers.js
@@ -0,0 +1,9 @@
+import { addCustomersListAction } from "../customerReducer";
+
+export const fetchCustomers = () => {
+  return function (dispatch) {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => dispatch(addCustomersListAction(json)));
+  };
+};
