418051ee8add9fa7821600aa7662a3a6fb5af5dc
solidados <geekbrains.pk@internet.ru>
2024-05-20 11:01:21 +0400

    feat: add hooks useDispatch and useSelector; add methods to apply onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
-  return <div className={"App"}>App</div>;
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+
+  return (
+    <div className={"App"}>
+      <h1>Best Bank</h1>
+      <h2>{cash}</h2>
+      <div className={"btn-wrapper"}>
+        <button onClick={() => getCash(Number(prompt()))}>Снять деньги</button>
+        <button onClick={() => addCash(Number(prompt()))}>Внести деньги</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
