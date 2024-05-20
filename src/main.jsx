036f29577749beaddac67dab855618350d61d175
solidados <geekbrains.pk@internet.ru>
2024-05-20 12:00:44 +0400

    refactor: update imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
-import App from "./App.jsx";
+import { index } from "./store";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={index}>
     <App />
   </Provider>,
 );
