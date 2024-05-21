66ee600e8e3caae7533b5272393e5bcedebb51b3
solidados <geekbrains.pk@internet.ru>
2024-05-21 19:42:30 +0400

    feat: create rootWatcher for an array of watchers to pass root to middleware in store

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,1 +1,8 @@
 /** Здесь будет проинициализирована Saga */
+import { all } from "redux-saga/effects";
+import { countWatcher } from "./countSaga.js";
+import { userWatcher } from "./userSaga.js";
+
+export function* rootWatcher() {
+  yield all([countWatcher(), userWatcher()]);
+}
